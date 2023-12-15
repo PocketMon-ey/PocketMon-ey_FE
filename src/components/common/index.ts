ea0c47942c334e171de1294d8fbf29e8de0eb784
htogether7 <htogether7@gmail.com>
2023-12-15 14:28:33 +0900

    :art: 큰 버튼 개발
    큰 버튼 개발

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@
 // export { default as 컴포넌트이름 } from './파일이름';
 export { default as Card } from './Card/Card';
 export { default as AddButton } from './Button/AddButton/AddButton';
+export { default as BigButton } from './Button/BigButton/BigButton';
