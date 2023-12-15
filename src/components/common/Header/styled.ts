952241affe5767e2964323e4c40cbcd7d6921b0b
htogether7 <htogether7@gmail.com>
2023-12-15 14:33:37 +0900

    :art: 전체 화면 헤더 개발
    전체 화면 헤더 개발

---
+++ src/components/common/Header/styled.ts
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ImageContainer = styled.img`
+  width: 20px;
+  height: 100%;
+  object-fit: fill;
+`;
+
+export const HeaderTitleContainer = styled.div`
+  width: 70%;
+  font-size: 20px;
+`;
