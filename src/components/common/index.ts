6d5a6175db94b2995369dabe4ac49b3559f428e1
htogether7 <htogether7@gmail.com>
2023-12-15 14:32:25 +0900

    :art: 작은 버튼 개발
    작은 버튼 개발

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,3 +3,4 @@
 export { default as Card } from './Card/Card';
 export { default as AddButton } from './Button/AddButton/AddButton';
 export { default as BigButton } from './Button/BigButton/BigButton';
+export { default as SmallButton } from './Button/SmallButton/SmallButton';
