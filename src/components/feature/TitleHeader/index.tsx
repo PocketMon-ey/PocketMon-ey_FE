fc2c3f4484a3427035949799adea2c579ba674fc
seyoung choi <sychoi0516@naver.com>
2023-12-17 15:36:40 +0900

    :sparkles: 페이지 설명 헤더 props 설정

--- src/components/feature/TitleHeader/index.tsx
+++ src/components/feature/TitleHeader/index.tsx
@@ -5,17 +5,12 @@ type Props = {
   secondS: string;
 };
 
-// const TextField = ({ title,firstS,secondS}: Props) => {
-const TitleHeader = () => {
+const TitleHeader = ({ title, firstS, secondS }: Props) => {
   return (
     <S.StyledBackground>
-      <S.StyledTitle>대출을 신청해볼까요?</S.StyledTitle>
-      <S.StyledFirstSentence>
-        대출사유와 함께 금액을 설정해주세요.
-      </S.StyledFirstSentence>
-      <S.StyledSecondSentence>
-        신청 후, 보호자가 승인하면 대출을 할 수 있어요!
-      </S.StyledSecondSentence>
+      <S.StyledTitle>{title}</S.StyledTitle>
+      <S.StyledFirstSentence>{firstS}</S.StyledFirstSentence>
+      <S.StyledSecondSentence>{secondS}</S.StyledSecondSentence>
     </S.StyledBackground>
   );
 };
--- src/pages/loan/ApplyLoan.tsx
+++ src/pages/loan/ApplyLoan.tsx
@@ -5,7 +5,11 @@ const ApplyLoan = () => {
   return (
     <>
       <Header headerTitle={'대출 신청'} />
-      <TitleHeader />
+      <TitleHeader
+        title={'대출을 신청해볼까요?'}
+        firstS={'대출사유와 함께 금액을 설정해주세요.'}
+        secondS={'신청 후, 보호자가 승인하면 대출을 할 수 있어요!'}
+      />
       <LoanInput />
     </>
   );
