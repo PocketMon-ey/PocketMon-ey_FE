ba76e42e64ac1babad5675af58ee4a940e5fafb0
htogether7 <htogether7@gmail.com>
2023-12-18 17:56:48 +0900

    :art: 부모 대출 목록에서 금리 조정 페이지로 이동하도록 개발
    금리 조정 페이지 navigate 개발

--- src/components/common/UserTab/styled.ts
+++ src/components/common/UserTab/styled.ts
@@ -10,3 +10,20 @@ export const UserTabContainer = styled.div`
   padding: 10px;
   margin-top: 20px;
 `;
+
+export const EditButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.gray_003};
+  border-radius: 10px;
+  width: 80px;
+  height: 20px;
+  text-align: center;
+`;
+
+export const UserTabHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
