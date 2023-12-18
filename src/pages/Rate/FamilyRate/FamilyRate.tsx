5161b7754bcdfa9fb46d0aab93c729498a4d16a8
htogether7 <htogether7@gmail.com>
2023-12-18 17:42:29 +0900

    :art: 가족금리 설정 페이지 개발
    가족금리 설정 페이지

--- src/pages/Rate/FamilyRate/FamilyRate.tsx
+++ src/pages/Rate/FamilyRate/FamilyRate.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import { RateParagraph, StyledTextFiled } from '../styled';
+import { BigButton } from '../../../components/common';
 const FamilyRate = () => {
-  return <div></div>;
+  return (
+    <div>
+      <RateParagraph>우리 가족의 현재 가족금리 : 5%</RateParagraph>
+      <RateParagraph>
+        수정 후 우리 가족의 가족금리 :
+        <br />
+        <StyledTextFiled placeholder="5"></StyledTextFiled>%
+      </RateParagraph>
+      <BigButton text="완료" />
+    </div>
+  );
 };
 
 export default FamilyRate;
