cf4dd8087fde731ef6885a10c5d241a957c3497c
htogether7 <htogether7@gmail.com>
2023-12-18 11:25:48 +0900

    :bug: 홈 버튼 오류 수정 및 라우팅 수정
    홈 버튼 오류 수정 및 라우팅 수정

--- src/pages/ChildHome/ChildHome.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ChildHome = () => {
-  return <div></div>;
-};
-
-export default ChildHome;
--- src/pages/ParentHome/ParentHome.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ParentHome = () => {
-  return <div></div>;
-};
-
-export default ParentHome;
