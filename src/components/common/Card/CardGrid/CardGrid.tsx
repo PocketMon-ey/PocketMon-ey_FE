0b1d1345a5d57e25d64e6d190ce88ce44223cb12
htogether7 <htogether7@gmail.com>
2023-12-21 15:19:05 +0900

    :art: 리액트 쿼리를 활용하여 금리 + 대출 목록 가져오기

--- src/components/common/Card/CardGrid/CardGrid.tsx
+++ src/components/common/Card/CardGrid/CardGrid.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { CardGridContainer } from './styled';
 import Card from '../Card';
 import { LoanListContext } from '../../../../context/context';
+import { loanServiceAxiosInstance } from '../../../../core/api/axios';
+import { useQuery } from '@tanstack/react-query';
 
 type Loan = {
   id: number;
@@ -12,12 +14,29 @@ type Loan = {
 };
 
 const CardGrid = ({ status }: { status: number }) => {
-  const list = useContext(LoanListContext).filter(
-    (loan: Loan) => loan.status === status,
-  );
+  const fetchLoanList = async () =>
+    await loanServiceAxiosInstance
+      .get(`/loan/list/${status}`)
+      .then((res) => res.data);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: [`loan${status}`],
+    queryFn: () => fetchLoanList(),
+  });
+
+  if (isLoading) {
+    return <div>Loading중</div>;
+  }
+
+  if (error) {
+    return <div>에러!!</div>;
+  }
+  // const list = useContext(LoanListContext).filter(
+  //   (loan: Loan) => loan.status === status,
+  // );
   return (
     <CardGridContainer>
-      {list.map((loan: Loan) => (
+      {data.loanList.map((loan: Loan) => (
         <Card
           key={loan.id}
           id={loan.id}
--- src/pages/LoanList/LoanList.tsx
+++ src/pages/LoanList/LoanList.tsx
@@ -3,21 +3,53 @@ import { AddButton, Header, TabBar, UserTab } from '../../components/common';
 import CardGrid from '../../components/common/Card/CardGrid/CardGrid';
 import { AddButtonContainer } from './styled';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { userServiceAxiosInstance } from '../../core/api/axios';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+type Props = {
+  familyrate: number;
+  primerate: number;
+};
 
 const LoanList = () => {
   const [status, setStatus] = useState<number>(0); // 임의로 0으로 해놓음
+
   const navigate = useNavigate();
   const path = useLocation().pathname;
+
+  const fetchFamilyRate = () => userServiceAxiosInstance.get(`/user/FR/1`);
+
+  const fetchPrimeRate = () => userServiceAxiosInstance.get(`/user/PIR/2`);
+
+  const fetchRate = async () =>
+    await axios.all([fetchFamilyRate(), fetchPrimeRate()]).then(
+      axios.spread((res1, res2) => {
+        const response1 = res1.data;
+        const response2 = res2.data;
+
+        const res = { familyrate: response1, primerate: response2 };
+        return res;
+      }),
+    );
+
+  const { data, isLoading, error } = useQuery<Props>({
+    queryKey: ['rate'],
+    queryFn: () => fetchRate(),
+  });
+
   return (
-    <div>
-      <Header headerTitle="대출 목록" />
-      <UserTab />
-      <TabBar status={status} setStatus={setStatus} />
-      <CardGrid status={status} />
-      <AddButtonContainer onClick={() => navigate('/child/loan/apply')}>
-        {path.includes('child') ? <AddButton></AddButton> : <></>}
-      </AddButtonContainer>
-    </div>
+    <>
+      <div>
+        <Header headerTitle="대출 목록" />
+        <UserTab familyrate={data?.familyrate} primerate={data?.primerate} />
+        <TabBar status={status} setStatus={setStatus} />
+        <CardGrid status={status} />
+        <AddButtonContainer onClick={() => navigate('/child/loan/apply')}>
+          {path.includes('child') ? <AddButton></AddButton> : <></>}
+        </AddButtonContainer>
+      </div>
+    </>
   );
 };
 
